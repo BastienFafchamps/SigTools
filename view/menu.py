76133900d68c5a83fa40ccef5b367dd39a689ba4
FAFCHAMPS Bastien <b.fafchamps@student.helmo.be>
2021-05-25 23:18:26 +0200

    Added Menu

--- view/menu.py
+++ view/menu.py
@@ -5,6 +5,21 @@ class Menu(QMenuBar):
     def __init__(self, parent, on_inspector, on_items, on_json_editor):
         super().__init__(parent)
 
+        files_menu = QMenu("&Files", self)
+        self.addMenu(files_menu)
+        
+        new_mod = QAction('New Mod', self)
+        # new_mod.triggered.connect(on_new_mod)
+        files_menu.addAction(new_mod)
+
+        save_mod = QAction('Save Mod', self)
+        # save_mod.triggered.connect(on_save_mod)
+        files_menu.addAction(save_mod)
+
+        save_mod_as = QAction('Save Mod As', self)
+        # save_mod_as.triggered.connect(on_save_mod_as)
+        files_menu.addAction(save_mod_as)
+
         views_menu = QMenu("&Views", self)
         self.addMenu(views_menu)
 
